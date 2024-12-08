700ce91002800d726c6011be75685fac2ba9f5ac
Lucas L. Soutto <lucas.leonildo@hotmail.com>
2024-12-08 19:05:45 -0300

    Criando csproj do client e do backgroundtasks

---
+++ Client/AssetClient.cs
@@ -0,0 +1,23 @@
+using System.Text.Json;
+
+namespace Client
+{
+    public class AssetClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public AssetClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetDataAsync()
+        {
+            var response = await _httpClient.GetAsync("https://localhost:5001/api/data");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<string>(content);
+        }
+    }
+}
